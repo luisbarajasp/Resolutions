9ec1094d18bf4f129a2ab9236e028ced07329076
luisbarajasp <luisbarajasp@gmail.com>
2016-02-09 14:54:52 -0600

    Add: functionality for adding the resolutions via form, add checkbox for finished resolutions, and checkbox for hiding finished

--- resolutions.js
+++ resolutions.js
@@ -1,10 +1,43 @@
+Resolutions = new Mongo.Collection('resolutions');
+
 if (Meteor.isClient) {
   Template.body.helpers({
-      resolutions: [
-          { title: "Hello Resolution #1" },
-          { title: "Bye Resolution #2" },
-          { title: "Kanye West" }
-      ]
+      resolutions: function() {
+          if (Session.get('hideFinished')){
+              return Resolutions.find({checked: {$ne: true}});
+          }else{
+              return Resolutions.find();
+          }
+      },
+      hideFinished: function() {
+          return Session.get('hideFinished');
+      }
+  });
+  Template.body.events({
+     'submit .new-resolution': function(event) {
+         var title = event.target.title.value;
+
+         Resolutions.insert({
+            title: title,
+            createdAt: new Date()
+         });
+
+         event.target.title.value = "";
+         return false;
+     },
+     'change .hide-finished': function(event){
+         Session.set('hideFinished', event.target.checked);
+     }
+  });
+  Template.resolution.events({
+     'click .toggle-checked': function(){
+         Resolutions.update(this._id, {$set:{
+             checked: !this.checked
+         }});
+     },
+     'click .delete': function(){
+        Resolutions.remove(this._id);
+     }
   });
 }
 
